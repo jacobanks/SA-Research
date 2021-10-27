587fea3c8055dcaa6292a3382814c861dcbcdd41
Jacob Banks <jacobanks@gmail.com>
2021-10-27 15:58:06 -0400

    Updates

--- nlp_test.py
+++ nlp_test.py
@@ -1,5 +1,5 @@
 from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.corpus import twitter_samples, stopwords
+from nltk.corpus import movie_reviews, stopwords
 from nltk.tag import pos_tag
 from nltk.tokenize import word_tokenize
 from nltk import FreqDist, classify, NaiveBayesClassifier
@@ -10,7 +10,8 @@ import csv
 import emoji
 import json
 import contractions
-        
+import pytreebank
+
 def process_data(tweet_tokens, stop_words = ()):
     cleaned_tokens = []
 
@@ -34,19 +35,19 @@ def process_data(tweet_tokens, stop_words = ()):
 
         if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words and token not in ["..."]:
             cleaned_tokens.append(token.lower())
-    
-#    bigram_tokens = []
-#    for item in bigrams(cleaned_tokens):
-#            bigram_tokens.append(' '.join(item))
-      
+
+    # bigram_tokens = []
+    # for item in bigrams(cleaned_tokens):
+    #     bigram_tokens.append(' '.join(item))
+
     return cleaned_tokens
 
-def get_tweets_for_model(cleaned_tokens_list):
+def format_for_model(cleaned_tokens_list):
     dataset = []
     for tweet_tokens in cleaned_tokens_list:
         dataset.append((dict([token, True] for token in tweet_tokens[0]), tweet_tokens[1]))
     return dataset
-        
+
 def classify_comment_list(comments):
     total_positive = 0
     total_negative = 0
@@ -55,31 +56,45 @@ def classify_comment_list(comments):
     subreddits = []
     stop_words = stopwords.words('english')
 
+    length = []
+    
     for comment in comments:
+        length.append(len(word_tokenize(comment["body"])))
         # process comment
         input_text = contractions.fix(comment["body"])
         custom_tokens = process_data(word_tokenize(input_text), stop_words)
         # add tokens and subreddits for freq dist
         all_tokens.extend(custom_tokens)
-        subreddits.append(comment["subreddit"])
+         subreddits.append(comment["subreddit"])
         
-        # Classify tokens
+         Classify tokens
         sa_value = classifier.prob_classify(dict([token, True] for token in custom_tokens))
         positive = sa_value.prob("Positive")
         negative = sa_value.prob("Negative")
-        if not negative > 0.05:
+        print(negative)
+        print(positive)
+        if not negative > 0.5:
             if positive - negative > 0:
                 total_positive += 1
+                print(comment + " --------- Positive")
             else:
                 total_neutral += 1
-        elif not positive > 0.05:
+                print(comment + " --------- Neutral")
+        elif not positive > 0.5:
             if positive - negative <= 0:
                 total_negative += 1
+                print(comment + " --------- Negative")
             else:
                 total_neutral += 1
+                print(comment + " --------- Neutral")
         else:
             total_neutral += 1
-        
+            print(comment + " --------- Neutral")
+
+    freq_dist_pos = FreqDist(length)
+    print("\nMost Common Tokens: ")
+    print(freq_dist_pos.most_common(10))
+    print(max(length))
     print("Positive: " + str(total_positive) + " -------- " + "Negative: " + str(total_negative) + " -------- " + "Neutral: " + str(total_neutral))
     freq_dist_pos = FreqDist(all_tokens)
     print("\nMost Common Tokens: ")
@@ -90,43 +105,61 @@ def classify_comment_list(comments):
     print("-----------------------------------------")
 
 if __name__ == "__main__":
+    stop_words = stopwords.words('english')
+
     print("Loading Training Data...")
     training_data_file = open('trainingandtestdata/training.1600000.processed.noemoticon.csv', encoding='latin1')
     unprocesses_data = list(csv.reader(training_data_file))
-#    pos = unprocesses_data[:10000]
-#    neg = unprocesses_data[-10000:]
-#    unprocesses_data = pos + neg
-    
+    pos = unprocesses_data[:100000]
+    neg = unprocesses_data[-100000:]
+    unprocesses_data = pos + neg
+
     # Split into positive and negative and tokenize/expand contractions
+    print("Preprocessing Training Data...")
     training_data = []
+    split = []
     for row in unprocesses_data:
         if row[0] == "0":
-            training_data.append([word_tokenize(contractions.fix(row[5])), "Negative"])
+            split.append("Negative")
+            tokens = word_tokenize(contractions.fix(row[5]))
+            training_data.append([process_data(tokens, stop_words), "Negative"])
         elif row[0] == "4":
-            training_data.append([word_tokenize(contractions.fix(row[5])), "Positive"])
+            split.append("Positive")
+            tokens = word_tokenize(contractions.fix(row[5]))
+            training_data.append([process_data(tokens, stop_words), "Positive"])
 
-    stop_words = stopwords.words('english')
-    cleaned_tokens_list = []
+    freq_dist_pos = FreqDist(split)
+    print("Positive / Negative Split: ")
+    print(freq_dist_pos.most_common(10))
 
-    # Process data by removing stop words, lemmatizing
-    print("Preprocessing Training Data...")
-    for row in training_data:
-        tokens = row[0]
-        cleaned_tokens_list.append([process_data(tokens, stop_words), row[1]])
+    with open('processed_training.txt', 'w') as outfile:
+        outfile.write(str(training_data))
+
+    print("Loading and Preprocessing Testing Data...")
+    dataset = pytreebank.load_sst("trees/")
+    testing_data = []
+    for sentence in dataset["test"]:
+        label = sentence.to_labeled_lines()[0][0]
+        text = sentence.to_labeled_lines()[0][1]
+        if label <= 1:
+            tokens = word_tokenize(contractions.fix(text))
+            testing_data.append([process_data(tokens, stop_words), "Negative"])
+        elif label >= 3:
+            tokens = word_tokenize(contractions.fix(text))
+            testing_data.append([process_data(tokens, stop_words), "Positive"])
 
     # Format for Model
-    dataset = get_tweets_for_model(cleaned_tokens_list)
-    random.shuffle(dataset)
-    
-    # Split training and test data
-    train_data = dataset[:1280000]
-    test_data = dataset[-320000:]
+    print("Formatting for Model...")
+    training_data = format_for_model(training_data)
+    testing_data = format_for_model(testing_data)
+    random.shuffle(training_data)
+    random.shuffle(testing_data)
 
     # Train Model
     print("Training Model...\n")
-    classifier = NaiveBayesClassifier.train(train_data)
+    classifier = NaiveBayesClassifier.train(training_data)
 
-    print("Accuracy is:", str(classify.accuracy(classifier, test_data) * 100) + "%")
+    print("Accuracy is:", str(classify.accuracy(classifier, testing_data) * 100) + "%")
     print(classifier.show_most_informative_features(10))
 
     # Run experiments
@@ -134,10 +167,11 @@ if __name__ == "__main__":
     print("-----------------------------------------")
     biden_file = open('experiment-data/biden_comments.json',)
     biden_submissions = json.load(biden_file)
+#    biden_submissions = ["I really like Biden.", "Biden is awesome", "I don't know about Biden.", "Biden is terrible!", "I don't like Biden."]
     classify_comment_list(biden_submissions)
-    
-    print("\nClassifying Trump Comments...")
-    print("-----------------------------------------")
+
+#    print("\nClassifying Trump Comments...")
+#    print("-----------------------------------------")
     trump_file = open('experiment-data/trump_comments.json',)
     trump_submissions = json.load(trump_file)
     classify_comment_list(trump_submissions)
