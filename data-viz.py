ef6f5c5192218a7bad9b8b6d898adc451319c0ae
jbanks <jacob_banks@taylor.edu>
2021-12-04 23:34:59 -0500

    images

--- data-viz.py
+++ data-viz.py
@@ -41,26 +41,34 @@ def scale_sentiment(result, polarity_sum):
     return polarity_sum
 
 times = []
-files = ['Data/Twitter/scores/biden_scores_final.json', 'Data/Twitter/scores/trump_scores_final.json']
-polarity_sum_trump = {"V. Negative": 0, "Negative": 0, "Neutral": 0, "Positive": 0, "V. Positive": 0}
-polarity_sum_biden = {"V. Negative": 0, "Negative": 0, "Neutral": 0, "Positive": 0, "V. Positive": 0}
-polarity_sum_both = {"V. Negative": 0, "Negative": 0, "Neutral": 0, "Positive": 0, "V. Positive": 0}
+files = ['Data/Twitter/scores/biden_scores_final.json', 'Data/Twitter/scores/trump_scores_final.json', 'Data/Reddit/scores/biden_scores_final.json', 'Data/Reddit/scores/trump_scores_final.json']
+# polarity_sum_trump = {"V. Negative": 0, "Negative": 0, "Neutral": 0, "Positive": 0, "V. Positive": 0}
+polarity_sum_biden_twitter = {"V. Negative": 0, "Negative": 0, "Neutral": 0, "Positive": 0, "V. Positive": 0}
+polarity_sum_biden_reddit = {"V. Negative": 0, "Negative": 0, "Neutral": 0, "Positive": 0, "V. Positive": 0}
+# polarity_sum_both = {"V. Negative": 0, "Negative": 0, "Neutral": 0, "Positive": 0, "V. Positive": 0}
+candidates = ["trump", "biden"]
+sites = ["Twitter", "Reddit"]
+# for file in files:
+for candidate in candidates:
+    for site in sites:
+        polarity_scores = json.load(open('Data/' + site + '/scores/' + candidate + '_scores_final.json',))
+        for data_sample in polarity_scores:
+            for data in data_sample["mentions"]:
+                if "score" in data:
+                    result = data["score"]
+                else:
+                    result = data_sample["score"]
+        
+                if data["topic"] == "trump":
+                    if site == "Twitter":
+                        polarity_sum_biden_twitter = scale_sentiment(result, polarity_sum_biden_twitter)
+                    else:
+                        polarity_sum_biden_reddit = scale_sentiment(result, polarity_sum_biden_reddit)
 
-for file in files:
-    polarity_scores = json.load(open(file,))
-    for data_sample in polarity_scores:
-        for data in data_sample["mentions"]:
-            if "score" in data:
-                result = data["score"]
-            else:
-                result = data_sample["score"]
-    
-        if data["topic"] == "biden":
-            polarity_sum_biden = scale_sentiment(result, polarity_sum_biden)
-        elif data["topic"] == "trump":
-            polarity_sum_trump = scale_sentiment(result, polarity_sum_trump)
-        elif data["topic"] == "both":
-            polarity_sum_both = scale_sentiment(result, polarity_sum_both)    
+        # elif data["topic"] == "trump":
+        #     polarity_sum_trump = scale_sentiment(result, polarity_sum_trump)
+        # elif data["topic"] == "both":
+        #     polarity_sum_both = scale_sentiment(result, polarity_sum_both)    
 
         # if data_sample["mentions"][0]["topic"] == "biden":
         #     polarity_sum_biden = scale_sentiment(result, polarity_sum_biden)
@@ -69,64 +77,78 @@ for file in files:
         # elif data_sample["mentions"][0]["topic"] == "both":
         #     polarity_sum_both = scale_sentiment(result, polarity_sum_both)       
 
-trump_total = sum(polarity_sum_trump.values())
-for key, value in polarity_sum_trump.items():
+polarity_sum_biden_twitter_percent = {"V. Negative": 0, "Negative": 0, "Neutral": 0, "Positive": 0, "V. Positive": 0}
+polarity_sum_biden_reddit_percent = {"V. Negative": 0, "Negative": 0, "Neutral": 0, "Positive": 0, "V. Positive": 0}
+trump_total = sum(polarity_sum_biden_reddit.values())
+for key, value in polarity_sum_biden_reddit.items():
     if key == "V. Negative":
         print(key + ": " + str(value / trump_total * 100) + "%")
+        polarity_sum_biden_reddit_percent = {"V. Negative": value / trump_total * 100, "Negative": polarity_sum_biden_reddit_percent["Negative"], "Neutral": polarity_sum_biden_reddit_percent["Neutral"], "Positive": polarity_sum_biden_reddit_percent["Positive"], "V. Positive": polarity_sum_biden_reddit_percent["V. Positive"]}
     elif key == "Negative":
         print(key + ": " + str(value / trump_total * 100) + "%")
+        polarity_sum_biden_reddit_percent = {"V. Negative": polarity_sum_biden_reddit_percent["V. Negative"], "Negative": value / trump_total * 100, "Neutral": polarity_sum_biden_reddit_percent["Neutral"], "Positive": polarity_sum_biden_reddit_percent["Positive"], "V. Positive": polarity_sum_biden_reddit_percent["V. Positive"]}
     elif key == "Neutral":
         print(key + ": " + str(value / trump_total * 100) + "%")
+        polarity_sum_biden_reddit_percent = {"V. Negative": polarity_sum_biden_reddit_percent["V. Negative"], "Negative": polarity_sum_biden_reddit_percent["Negative"], "Neutral": value / trump_total * 100, "Positive": polarity_sum_biden_reddit_percent["Positive"], "V. Positive": polarity_sum_biden_reddit_percent["V. Positive"]}
     elif key == "Positive":
         print(key + ": " + str(value / trump_total * 100) + "%")
+        polarity_sum_biden_reddit_percent = {"V. Negative": polarity_sum_biden_reddit_percent["V. Negative"], "Negative": polarity_sum_biden_reddit_percent["Negative"], "Neutral": polarity_sum_biden_reddit_percent["Neutral"], "Positive": value / trump_total * 100, "V. Positive": polarity_sum_biden_reddit_percent["V. Positive"]}
     elif key == "V. Positive":
         print(key + ": " + str(value / trump_total * 100) + "%")
-print("Trump sents: {}\n".format(polarity_sum_trump))
-print("Trump Sum: {}\n".format(trump_total))
+        polarity_sum_biden_reddit_percent = {"V. Negative": polarity_sum_biden_reddit_percent["V. Negative"], "Negative": polarity_sum_biden_reddit_percent["Negative"], "Neutral": polarity_sum_biden_reddit_percent["Neutral"], "Positive": polarity_sum_biden_reddit_percent["Positive"], "V. Positive": value / trump_total * 100}
 
-biden_total = sum(polarity_sum_biden.values())
-for key, value in polarity_sum_biden.items():
+print("Reddit sents: {}".format(polarity_sum_biden_reddit_percent))
+print("Reddit Sum: {}\n".format(trump_total))
+
+biden_total = sum(polarity_sum_biden_twitter.values())
+for key, value in polarity_sum_biden_twitter.items():
     if key == "V. Negative":
         print(key + ": " + str(value / biden_total * 100) + "%")
+        polarity_sum_biden_twitter_percent = {"V. Negative": value / biden_total * 100, "Negative": polarity_sum_biden_twitter_percent["Negative"], "Neutral": polarity_sum_biden_twitter_percent["Neutral"], "Positive": polarity_sum_biden_twitter_percent["Positive"], "V. Positive": polarity_sum_biden_twitter_percent["V. Positive"]}
     elif key == "Negative":
         print(key + ": " + str(value / biden_total * 100) + "%")
+        polarity_sum_biden_twitter_percent = {"V. Negative": polarity_sum_biden_twitter_percent["V. Negative"], "Negative": value / biden_total * 100, "Neutral": polarity_sum_biden_twitter_percent["Neutral"], "Positive": polarity_sum_biden_twitter_percent["Positive"], "V. Positive": polarity_sum_biden_twitter_percent["V. Positive"]}
     elif key == "Neutral":
         print(key + ": " + str(value / biden_total * 100) + "%")
+        polarity_sum_biden_twitter_percent = {"V. Negative": polarity_sum_biden_twitter_percent["V. Negative"], "Negative": polarity_sum_biden_twitter_percent["Negative"], "Neutral": value / biden_total * 100, "Positive": polarity_sum_biden_twitter_percent["Positive"], "V. Positive": polarity_sum_biden_twitter_percent["V. Positive"]}
     elif key == "Positive":
         print(key + ": " + str(value / biden_total * 100) + "%")
+        polarity_sum_biden_twitter_percent = {"V. Negative": polarity_sum_biden_twitter_percent["V. Negative"], "Negative": polarity_sum_biden_twitter_percent["Negative"], "Neutral": polarity_sum_biden_twitter_percent["Neutral"], "Positive": value / biden_total * 100, "V. Positive": polarity_sum_biden_twitter_percent["V. Positive"]}
     elif key == "V. Positive":
         print(key + ": " + str(value / biden_total * 100) + "%")
-print("Biden sents: {}\n".format(polarity_sum_biden))
-print("Biden Sum: {}\n".format(biden_total))
+        polarity_sum_biden_twitter_percent = {"V. Negative": polarity_sum_biden_twitter_percent["V. Negative"], "Negative": polarity_sum_biden_twitter_percent["Negative"], "Neutral": polarity_sum_biden_twitter_percent["Neutral"], "Positive": polarity_sum_biden_twitter_percent["Positive"], "V. Positive": value / biden_total * 100}
 
-both_total = sum(polarity_sum_both.values())
-for key, value in polarity_sum_both.items():
-    if key == "V. Negative":
-        print(key + ": " + str(value / both_total * 100) + "%")
-    elif key == "Negative":
-        print(key + ": " + str(value / both_total * 100) + "%")
-    elif key == "Neutral":
-        print(key + ": " + str(value / both_total * 100) + "%")
-    elif key == "Positive":
-        print(key + ": " + str(value / both_total * 100) + "%")
-    elif key == "V. Positive":
-        print(key + ": " + str(value / both_total * 100) + "%")
-print("Both sents: {}\n".format(polarity_sum_both))
-print("Both Sum: {}\n".format(both_total))
+print("Twitter sents: {}".format(polarity_sum_biden_twitter_percent))
+print("Twitter Sum: {}\n".format(biden_total))
 
-names = list(polarity_sum_trump.keys())
-values_trump = list(polarity_sum_trump.values())
-values_biden = list(polarity_sum_biden.values())
-values_both = list(polarity_sum_both.values())
+# both_total = sum(polarity_sum_both.values())
+# for key, value in polarity_sum_both.items():
+#     if key == "V. Negative":
+#         print(key + ": " + str(value / both_total * 100) + "%")
+#     elif key == "Negative":
+#         print(key + ": " + str(value / both_total * 100) + "%")
+#     elif key == "Neutral":
+#         print(key + ": " + str(value / both_total * 100) + "%")
+#     elif key == "Positive":
+#         print(key + ": " + str(value / both_total * 100) + "%")
+#     elif key == "V. Positive":
+#         print(key + ": " + str(value / both_total * 100) + "%")
+# print("Both sents: {}\n".format(polarity_sum_both))
+# print("Both Sum: {}\n".format(both_total))
+
+names = list(polarity_sum_biden_twitter_percent.keys())
+values_trump = list(polarity_sum_biden_reddit_percent.values())
+values_biden = list(polarity_sum_biden_twitter_percent.values())
+# values_both = list(polarity_sum_both.values())
 
 fig, axs = plt.subplots(1, 1, figsize=(5, 4), sharey=True)
-axs.bar(names, values_biden, color=(0.2, 0.4, 0.7, 0.6), label="Biden")
-axs.bar(names, values_trump, color=(0.6, 0.1, 0.1, 0.6), bottom=values_biden, label="Trump")
-values_trump = np.array(values_trump)
-values_biden = np.array(values_biden)
-axs.bar(names, values_both, color=(0.4, 0.4, 0.4, 0.6), bottom=values_trump+values_biden, label="Both")
+axs.bar(names, values_biden, color=(0.2, 0.4, 0.7, 0.6), label="Twitter")
+axs.bar(names, values_trump, color=(0.6, 0.1, 0.1, 0.6), bottom=values_biden, label="Reddit")
+# values_trump = np.array(values_trump)
+# values_biden = np.array(values_biden)
+# axs.bar(names, values_both, color=(0.4, 0.4, 0.4, 0.6), bottom=values_trump+values_biden, label="Both")
 axs.legend()
 
-plt.title('Candidate Sentiment Distribution Twitter')
+plt.title('Sentiment Distribution Percentage Trump')
 
 plt.show()
